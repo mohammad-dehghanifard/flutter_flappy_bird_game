5089486b3b4fe26821883e7f33bda8a6598fe1f3
mohammad <m.deheghanifard@gmail.com>
2024-02-28 08:31:33 +0330

    Initial commit

---
+++ android/app/src/main/kotlin/ir/dehghanifard/flutter_flappy_bird_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package ir.dehghanifard.flutter_flappy_bird_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/resource/assets.dart
@@ -0,0 +1,19 @@
+class GameAssets {
+  static const gameBackGround = "background.png";
+  static const clouds = "clouds.png";
+  static const ground = "ground.png";
+  static const pipe = "pipe.png";
+  static const rotatedPipe = "pipe_rotated.png";
+
+  static const birdMid = "bird_midflap.png";
+  static const birdUp = "bird_upflap.png";
+  static const birdDown = "bird_downflap.png";
+
+  static const gameOver = 'assets/images/gameover.png';
+  static const menu = 'assets/images/menu.jpg';
+  static const message = 'assets/images/message.png';
+
+  static const flying = 'fly.wav';
+  static const collision = 'collision.wav';
+  static const point = 'point.wav';
+}
